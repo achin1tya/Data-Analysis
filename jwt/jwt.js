2d256662c3f56e95af025ca74eb1f71fe5d30853
Achintya <achin1tya@gmail.com>
2018-08-03 16:42:06 +0530

    Added The final Push for NSDA

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -1,39 +1,10 @@
-var header = {
-  "alg": "HS256",
-  "typ": "JWT"
-};
 
-var data = {
-  "id": 1337,
-  "username": "john.doe"
-};
+import * from "jsonwebtoken"
 
-var secret = "My very confidential secret!!!";
 
-function base64url(source) {
-  // Encode in classical base64
-  encodedSource = CryptoJS.enc.Base64.stringify(source);
-  
-  // Remove padding equal characters
-  encodedSource = encodedSource.replace(/=+$/, '');
-  
-  // Replace characters according to base64url specifications
-  encodedSource = encodedSource.replace(/\+/g, '-');
-  encodedSource = encodedSource.replace(/\//g, '_');
-  
-  return encodedSource;
-}
+var signature=jsonwebtoken.sign({
+  "username":"NSDA",
+  "password":"0000"
+}, 'secret', { expiresIn: '1h' });
 
-var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
-var encodedHeader = base64url(stringifiedHeader);
-document.getElementById("header").innerText = encodedHeader;
-
-var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
-var encodedData = base64url(stringifiedData);
-document.getElementById("payload").innerText = encodedData;
-
-var signature = encodedHeader + "." + encodedData;
-signature = CryptoJS.HmacSHA256(signature, secret);
-signature = base64url(signature);
-
-document.getElementById("signature").innerText = signature;
+document.getElementById("signature")=signature;
--- pdfToTextUsingMongo/src/main/java/WorkingWithJWT.java
+++ pdfToTextUsingMongo/src/main/java/WorkingWithJWT.java
@@ -1,39 +1,59 @@
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jws;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//
+//import java.io.UnsupportedEncodingException;
+//import java.util.Date;
+//
+//public void createJWT(){
+//        try {
+//        String jwt = Jwts.builder()
+//        .setSubject("users/TzMUocMF4p")
+//        .setExpiration(new Date(1300819380))
+//        .claim("username", "NSDA")
+//        .claim("scope", "self groups/admins")
+//        .signWith(
+//        SignatureAlgorithm.HS256,
+//        "secret".getBytes("UTF-8")
+//
+//public class WorkingWithJWT {                    )
+//                    .compact();
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//    }
+////    public void validatJWT(String jwtParameter) throws UnsupportedEncodingException {
+////        String jwt = jwtParameter;
+////        Jws<Claims> claims = Jwts.parser()
+////                .setSigningKey("secret".getBytes("UTF-8"))
+////                .parseClaimsJws(jwt);
+////        String scope = (String) claims.getBody().get("scope");
+////        assertEquals(scope, "self groups/admins");
+////
+////    }
+//    public static void main(String[] args) throws UnsupportedEncodingException {
+//        WorkingWithJWT j1=new WorkingWithJWT();
+//        j1.createJWT();
+//    }
+//}
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider; //TODO: clean up via https://github.com/jwtk/jjwt/issues/350
+import java.security.Key;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-
-public class WorkingWithJWT {
-    public void createJWT(){
-        try {
-            String jwt = Jwts.builder()
-                    .setSubject("users/TzMUocMF4p")
-                    .setExpiration(new Date(1300819380))
-                    .claim("username", "NSDA")
-                    .claim("scope", "self groups/admins")
-                    .signWith(
-                            SignatureAlgorithm.HS256,
-                            "secret".getBytes("UTF-8")
-                    )
-                    .compact();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+class WorkingWithJWT{
+   public static String  createJwt(){
+        Key key = MacProvider.generateKey();
+        String compactJws = Jwts.builder()
+                .setSubject("Joe")
+                .signWith(SignatureAlgorithm.HS512, key)
+                .compact();
+        return compactJws;
     }
-//    public void validatJWT(String jwtParameter) throws UnsupportedEncodingException {
-//        String jwt = jwtParameter;
-//        Jws<Claims> claims = Jwts.parser()
-//                .setSigningKey("secret".getBytes("UTF-8"))
-//                .parseClaimsJws(jwt);
-//        String scope = (String) claims.getBody().get("scope");
-//        assertEquals(scope, "self groups/admins");
-//
-//    }
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        WorkingWithJWT j1=new WorkingWithJWT();
-        j1.createJWT();
+
+    public static void main(String[] args) {
+        String ans=createJwt();
+        System.out.println(ans);
     }
 }
